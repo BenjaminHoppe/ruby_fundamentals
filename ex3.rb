6a3eec863d9303a78dd62e4bfffc69523197ad38
BenjaminHoppe <38235089+BenjaminHoppe@users.noreply.github.com>
2018-11-12 15:12:01 -0500

    Done exercise 3

---
+++ ex3.rb
@@ -0,0 +1,58 @@
+# puts "What is your name?"
+# print "> "
+# user_name = gets.chomp
+# puts "Hello, #{user_name} welcome to Monday."
+#
+# puts "How old are you? "
+# print "> "
+# user_age = gets.chomp.to_i
+#
+# puts "You were born in #{2018 - user_age} wow."
+
+
+
+# my_number = 1
+#
+# if my_number > 1
+#   puts "The number is greater than 1"
+# else
+#   puts "The number is less than 1"
+# end
+
+
+# puts "pick a number"
+# print "> "
+# number = gets.to_i
+#
+# if number > 0
+#   puts "#{number} is a positive number"
+# else
+#   puts "#{number} is a negative number"
+# end
+
+# puts "pick two numbers and well compare them"
+# puts "pick"
+# print "> "
+# x = gets.to_i
+# puts "pick another one, and we'll see which one's bigger"
+# print "> "
+# y = gets.to_i
+#
+# if x > y
+#   puts "x is greater than y!"
+# elsif x < y
+#   puts "x is less than y!"
+# else
+#   puts "x equals y!"
+# end
+
+
+x = 17
+
+if x != 10
+  puts "I get printed!"
+end
+
+unless x == 10
+  puts "I get printed"
+end
