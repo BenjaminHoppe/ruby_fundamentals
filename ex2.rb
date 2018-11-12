502d2fe84fe73e92726f30cac9336cfb65ac1467
BenjaminHoppe <38235089+BenjaminHoppe@users.noreply.github.com>
2018-11-12 14:37:00 -0500

    Done exercise 2

---
+++ ex2.rb
@@ -0,0 +1,14 @@
+# a way to tip
+puts "a way to tip"
+puts 55 * 0.15
+tip = 8.25
+puts "you should tip #{tip}."
+
+# adding a string and an intiger
+puts "I'm a string " + 3.to_s
+
+# multiplying big numbers
+puts "45628, multiplied by 7839 is equal to #{45628 * 7839}"
+
+# value of expression
+puts (10 < 20 && 30 < 20) || !(10 == 11)
